eea0046d4cb39d092f9fa2225cbe242a9c354bb2
icebamboo <ice_yang66@outlook.com>
2018-09-03 22:27:17 +0800

    20180903cc

---
+++ Template_lwip/BSP/LWIP/arch/bpstruct.h
@@ -0,0 +1,6 @@
+#ifndef __BPSTRUCT_H__
+#define __BPSTRUCT_H__
+
+//nothing, for Fatal Error[Pe1696]: cannot open source file "arch/bpstruct.h"
+
+#endif /* __BPSTRUCT_H__ */
---
+++ Template_lwip/BSP/LWIP/arch/epstruct.h
@@ -0,0 +1,6 @@
+#ifndef __EPSTRUCT_H__
+#define __EPSTRUCT_H__
+
+//nothing, for Fatal Error[Pe1696]: cannot open source file "arch/bpstruct.h"
+
+#endif /* __EPSTRUCT_H__ */
